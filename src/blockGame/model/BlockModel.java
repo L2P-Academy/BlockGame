f91c38c0b072c8dcb71f56e0fbe6deaa0bfb0392
Haubergsbiker <spiderman666@gmx.de>
2025-07-24 11:22:00 +0200

    Attribute für BlockModel angelegt - TG

---
+++ src/blockGame/model/BlockModel.java
@@ -0,0 +1,15 @@
+package blockGame.model;
+
+public class BlockModel {
+	
+	//Attributes
+	private String name;
+	private String material;
+	private int PosX;
+	private int PosY;
+	private bufferedImage texture;
+	private boolean isDestructable;
+	
+
+
+}
