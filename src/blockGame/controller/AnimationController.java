8adb091675cba09137576d8efb2682303e6e2baf
djvt <djvt@localhost>
2025-07-24 11:26:50 +0200

    AnimationController

---
+++ src/blockGame/controller/AnimationController.java
@@ -0,0 +1,15 @@
+package blockGame.controller;
+
+public class AnimationController {
+	
+	private String animations;
+	private void startAnimation() {
+		// TODO Auto-generated method stub
+
+	}
+	private void stopAnimation() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
