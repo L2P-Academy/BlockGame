63896adcb948cfad7f60112e0ed8b318a475ec8b
Kris-Ge144 <kristian-gebert@web.de>
2025-07-24 11:21:28 +0200

    Creating recipe models

---
+++ src/blockGame/model/recipeModel.java
@@ -0,0 +1,10 @@
+package blockGame.model;
+
+public class recipeModel {
+
+    private String name; 
+	private BlockModel typeofBlock; //BlockModel
+	private int numberOfBlocks;
+	private boolean isAvailable;
+	
+}
