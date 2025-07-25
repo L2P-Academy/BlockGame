67089baaac588234eddcbfdd1b5464b7771ae583
djvt <djvt@localhost>
2025-07-25 16:30:34 +0200

    This is an interface for anything that needs to be updated regularly

---
+++ src/blockGame/Updateable.java
@@ -0,0 +1,10 @@
+//MS, DJ
+
+Updatable.java
+package blockGame.common;
+
+/* This is an interface for anything that needs to be updated regularly (like every frame) */
+
+public interface Updatable {
+ void update(); // method to be called by the game loop
+}
