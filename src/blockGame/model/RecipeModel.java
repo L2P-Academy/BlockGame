ef8303fad55a80153e7d11478137490a475481be
Kris-Ge144 <kristian-gebert@web.de>
2025-07-25 11:04:50 +0200

    neue Platzhalter-Blöcke hinzugefügt. neue Rezepte hinzugefügt

--- src/blockGame/model/RecipeModel.java
+++ src/blockGame/model/RecipeModel.java
@@ -57,8 +57,28 @@ public class RecipeModel {
     //Platzhalter-Blöcke
     public static final BlockModel HOLZ = new BlockModel("Holz", "Naturmaterial", 0, 0, null, true);
     public static final BlockModel EISEN = new BlockModel("Eisen", "Metall", 0, 0, null, true);
+    public static final BlockModel WASSER = new BlockModel("Wasser", "Naturmaterial", 0, 0, null, true);
+    public static final BlockModel KRÄUTER = new BlockModel("Kräuter", "Naturmaterial", 0, 0, null, true);
+    public static final BlockModel ROHR = new BlockModel("Rohr", "Metall", 0, 0, null, true);
+    public static final BlockModel BOHNEN = new BlockModel("Bohnen", "Naturmaterial", 0, 0, null, true);
+    public static final BlockModel ZAHNRAD = new BlockModel("Zahnrad", "Metall", 0, 0, null, true);
+    public static final BlockModel MOOS = new BlockModel("Moos", "Naturmaterial", 0, 0, null, true);
+    public static final BlockModel FLEISCH = new BlockModel("Fleisch", "Essen", 0, 0, null, true);
+    public static final BlockModel SCHÜSSEL = new BlockModel("Schüssel", "null", 0, 0, null, true);
+    
   
     RecipeModel schaufel = new RecipeModel("Schaufel", List.of(RecipeModel.HOLZ, RecipeModel.EISEN), List.of(1, 1));
+    RecipeModel messer = new RecipeModel("Messer", List.of(RecipeModel.HOLZ, RecipeModel.EISEN), List.of(1, 1));
+    RecipeModel zahnrad = new RecipeModel("Zahnrad", List.of(RecipeModel.EISEN), List.of(1));
+    RecipeModel schüssel = new RecipeModel("Schüssel", List.of(RecipeModel.HOLZ), List.of(1));
+    RecipeModel rohr = new RecipeModel("Rohr", List.of(RecipeModel.EISEN), List.of(1));
+    RecipeModel bohrer = new RecipeModel("Bohrer", List.of(RecipeModel.ROHR, RecipeModel.EISEN, RecipeModel.ZAHNRAD), List.of(1, 1, 1));
+    RecipeModel dosenöffner = new RecipeModel("Dosenöffner", List.of(RecipeModel.EISEN), List.of(1));
+    RecipeModel säge = new RecipeModel("Säge", List.of(RecipeModel.HOLZ, RecipeModel.EISEN), List.of(1, 1));
+    RecipeModel healthPotion = new RecipeModel("Health-Potion", List.of(RecipeModel.WASSER, RecipeModel.KRÄUTER, RecipeModel.MOOS), List.of(1, 1, 1));
+    RecipeModel eintopf = new RecipeModel("Eintopf", List.of(RecipeModel.FLEISCH, RecipeModel.BOHNEN, RecipeModel.SCHÜSSEL), List.of(1, 1, 1));
+    
+    
     
 
 }
