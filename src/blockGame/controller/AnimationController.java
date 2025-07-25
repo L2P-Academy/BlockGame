f8fa7d8bb531fe7e73987faeac5e93a88389eb65
djvt <djvt@localhost>
2025-07-25 09:39:58 +0200

    newAnimationController
    Der Controller bezieht sich auf zwei weitere Blöcke, noch nicht
    committed: Updatable.java GameLoop.java und Änderungen in main

--- src/blockGame/controller/AnimationController.java
+++ src/blockGame/controller/AnimationController.java
@@ -1,15 +1,37 @@
+
+
+AnimationController.java
 package blockGame.controller;
 
-public class AnimationController {
-	
-	private String animations;
-	private void startAnimation() {
-		// TODO Auto-generated method stub
+import blockGame.model.AnimationsModel;
+import blockGame.common.Updatable;
 
-	}
-	private void stopAnimation() {
-		// TODO Auto-generated method stub
+// The controller connects the game loop to the animation model
+// It decides when the animation should advance (based on isAnimating)
 
-	}
+public class AnimationController implements Updatable {
+    private final AnimationsModel model;
+    private boolean isAnimating = true;
 
+    // Constructor: we pass in the model this controller should control
+    public AnimationController(AnimationsModel model) {
+        this.model = model;
+    }
+
+    // This method is called by the game loop every frame
+    @Override
+    public void update() {
+        if (isAnimating) {
+            model.advanceFrame(); // tell the model to move to the next frame
+        }
+    }
+
+    // Optionally used to control whether the animation is running or not
+    public void startAnimation() {
+        isAnimating = true;
+    }
+
+    public void stopAnimation() {
+        isAnimating = false;
+    }
 }
