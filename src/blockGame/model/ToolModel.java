b58395cd2e6c010f4e291e4784b5c71a3e483710
SickAn7 <kai-erbert@web.de>
2025-07-30 12:13:44 +0200

    ItemM erbt an ToolM, InvenSlotM nutzt nur Item M

--- src/blockGame/model/ToolModel.java
+++ src/blockGame/model/ToolModel.java
@@ -2,50 +2,21 @@
 
 package blockGame.model;
 
-public class ToolModel {
+public class ToolModel extends ItemModel {
 
-	 //attributes
-		private int id;
-		private String tool;
-		private String textureImagePath;
+	 //attributes;
 		private int durability;
-		private String tier;
+		private int tier;
 
 	//Constructor
-	public ToolModel(int id, String tool, String textureImagePath, int durability, String tier) {
-	    this.id = id;
-	    this.tool = tool;
-	    this.textureImagePath = textureImagePath;
+	public ToolModel(int id, String itemTyp, String itemName, String textureImagePath, int durability, int tier) {    
+		super(id, itemTyp, itemName, textureImagePath);
 	    this.durability = durability;
 	    this.tier = tier;
 	
 	}
 
-		// Getter und Setter
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTool() {
-		return tool;
-	}
-
-	public void setTool(String tool) {
-		this.tool = tool;
-	}
-
-	public String getTextureImagePath() {
-		return textureImagePath;
-	}
-
-	public void setTextureImagePath(String textureImagePath) {
-		this.textureImagePath = textureImagePath;
-	}
-
+	
 	public int getDurability() {
 		return durability;
 	}
@@ -54,22 +25,25 @@ public class ToolModel {
 		this.durability = durability;
 	}
 
-	public String getTier() {
+	public int getTier() {
 		return tier;
 	}
 
-	public void setTier(String tier) {
+	public void setTier(int tier) {
 		this.tier = tier;
 	}
 	
 	@Override
 	public String toString() {
-	    return "Tool: " + tool + " (ID: " + id + ", Texture: " + textureImagePath + ")";}
+	    return "Item: " + getItemTyp() + " (ID: " + getId() + ", Thingname: " + getItemName() + ", Texture: " + getTextureImagePath() + ", Durability: " + durability + ", Tier: " + tier + ")";} 
 
 
-	public static final ToolModel Holzspitzhake = new ToolModel(101, "Holzspitzhake", "textures/holzspitzhake.png", 30, "Tier 1");
-	public static final ToolModel Steinspitzhake = new ToolModel(102, "Steinspitzhake", "textures/steinspitzhake.png", 60, "Tier 2");
-	public static final ToolModel Holzaxt = new ToolModel(103, "Holzaxt", "textures/holzaxt.png", 30, "Tier 1");
-	public static final ToolModel Steinaxt = new ToolModel(104, "Steinaxt", "textures/steinaxt.png", 60, "Tier 2");
+	public static final ToolModel Holzspitzhake = new ToolModel(101, "Werkzeug", "Holzspitzhacke", "textures/holzspitzhake.png", 30, 1);
+	public static final ToolModel Steinspitzhake = new ToolModel(102, "Werkzeug", "Steinspitzhacke", "textures/steinspitzhake.png", 60, 2);
+	//public static final ToolModel Holzaxt = new ToolModel(103, "Werkzeug", "Holzaxt", "textures/holzaxt.png", 30, 1);
+	//public static final ToolModel Steinaxt = new ToolModel(104, "Werkzeug", "Steinaxt", "textures/steinaxt.png", 60, 2);
 	
 }
+
+
+// quantity auf feste 1 für werkzeuge setzen, bei durabilitiy 0 werkzeug entvernen.
