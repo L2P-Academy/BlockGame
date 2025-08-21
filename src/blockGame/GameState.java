1399eaa1291109ca1caed3f3ca86c3883641745a
djvt <djvt@macbookpro.mshome.net>
2025-08-21 12:56:22 +0200

    Gamestate created

---
+++ src/blockGame/GameState.java
@@ -0,0 +1,26 @@
+// Dirk
+package blockGame;
+
+public class GameState {
+	public final int worldRows; // max Anzahl Zeilen
+	public final int worldCols; // max Anzahl Spalten
+	public final int playerRow; // Spielerposition (Zeile)
+	public final int playerCol; // Spielerposition (Spalte)
+	public final int blockId; // Typ-Id: Dirt, Stone, ...
+	public final int[][] blockCoordinates;
+	
+	// Constructor
+	public GameState(int worldRows, int worldCols, int blockId, int playerRow, 
+			int playerCol, int[][] blockCoordinates) {
+		this.worldRows = worldRows;
+		this.worldCols = worldCols;
+		this.playerRow = playerRow;
+		this.playerCol = playerCol;
+		this.blockId = blockId;
+		this.blockCoordinates = blockCoordinates;
+	}
+
+	
+	
+
+}
