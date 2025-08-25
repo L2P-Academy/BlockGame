71662acdda456a3732311b2adc21a2d22595d43f
SickAn7 <kai-erbert@web.de>
2025-08-25 11:09:40 +0200

    Craftingview Buttonsound und beautifyB + Dokumentation

--- src/blockGame/view/CraftingView.java
+++ src/blockGame/view/CraftingView.java
@@ -9,17 +9,32 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+
+import blockGame.controller.SoundController;
+
 import javax.swing.JScrollPane;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.util.Random;
+
+
 
 	public class CraftingView extends JFrame {
 		private JTable craftingTable;
-		private JButton craftButton;
+		private JButton craftButton, zurückButton;
 
+		
+		/**
+		 *  Settings for the Craftingwindow 
+		 *  @author Kai E
+		 */
+		
 
 	public CraftingView(){
 		setTitle("Crafting Menü");
@@ -46,16 +61,31 @@ import java.util.Random;
     	JScrollPane scrollPane = new JScrollPane(craftingTable);
     	add(scrollPane, BorderLayout.CENTER);
     
-    
+    	/**
+    	 * Funktions for the Craft- and Backbutton
+    	 *  @author Kai E
+    	 */
+    	
+    	// Buttonpanel
+    	JPanel buttonPanel = new JPanel(new BorderLayout());
+    	
     	// Craft-Button
     	craftButton = new JButton("Craften");
-    	add(craftButton, BorderLayout.SOUTH);
+    	StartMenuView.beautifyButton(craftButton);
+    	buttonPanel.add(craftButton, BorderLayout.WEST);
+    	
+    	// Zurück-Button
+    	zurückButton = new JButton("Zurück");
+    	StartMenuView.beautifyButton(zurückButton);
+    	buttonPanel.add(zurückButton, BorderLayout.EAST);
+    	
+    	add(buttonPanel, BorderLayout.SOUTH);
+    	
     
-    
-    	// Listener für den Button
+    	// CraftButton Funktion
     	craftButton.addActionListener(e -> {
     		int selectedRow = craftingTable.getSelectedRow();
-    		if (selectedRow != -1) {
+    		if (selectedRow >= 0) {
     			String item = (String) craftingTable.getValueAt(selectedRow, 0);
     			JOptionPane.showMessageDialog(this, 
     				item + " wurde gecraftet!",
@@ -68,6 +98,31 @@ import java.util.Random;
     				JOptionPane.WARNING_MESSAGE);
     		}
     	});   
+    	
+    	
+    	// Zurückbutton Funktion
+    	zurückButton.addActionListener(e -> {
+    		dispose();
+    	});
+    	
+    
+    	
+    	// ActionListener für Buttonsound
+        zurückButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SoundController soundController2 = new SoundController();
+				soundController2.playBtnSound(); 
+            }
+        });
+
+        craftButton.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		SoundController soundController3 = new SoundController();
+        		soundController3.playBtnSound();
+        	}
+        });
     
     	setLocationRelativeTo(null);
     	setVisible(true);
