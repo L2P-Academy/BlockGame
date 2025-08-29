d0bb03f966dd8c2a6eaddef882fdd23488b6744a
L2P-Academy <projekte@l2p-academy.com>
2025-08-29 12:02:14 +0200

    Inventory functionality added with sum

--- src/blockGame/controller/InventoryController.java
+++ src/blockGame/controller/InventoryController.java
@@ -2,6 +2,29 @@
 
 package blockGame.controller;
 
+import blockGame.model.InventoryModel;
+import blockGame.model.ItemModel;
+
 public class InventoryController {
-	// TODO: ItemModel & -View
+	private static InventoryController instance;
+	private InventoryModel inventory;
+	
+	public InventoryController() {
+		inventory = new InventoryModel();
+	}
+	
+	public static InventoryController getInstance() {
+		if (instance == null) {
+			instance = new InventoryController();
+		}
+		return instance;
+	}
+	
+	public InventoryModel getInventory() {
+		return inventory;
+	}
+	
+	public void addItem(ItemModel item, int amount) {
+		inventory.addItem(item, amount);
+	}
 }
